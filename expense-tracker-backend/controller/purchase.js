8f066ff5fb4e96497b3a4a3f74891573539d9ae8
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-02-12 23:52:41 +0530

    added premium features and styles
    added premium features and styles

--- expense-tracker-backend/controller/purchase.js
+++ expense-tracker-backend/controller/purchase.js
@@ -7,7 +7,7 @@ const purchasepremium = async (req, res) => {
     var rzp = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_KEY_SECRET,
-   
+
     });
     const amount = 2500;
 
