1d90d8b4db4da0358ce8c3bf5181f7f380a5b030
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-02-12 14:02:13 +0530

    Delete expense-tracker-backend directory

--- expense-tracker-backend/routes/signup.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const signupController = require('../controller/signup')
-
-router.post( '/', signupController.signup )
-
-module.exports = router
--- expense-tracker-backend/utils/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require ('sequelize')
-
-
-const sequelize = new Sequelize('expensetrackeraws' , 'root','root',{
-    dialect : 'mysql',
-    host :'localhost'
-})
-
-module.exports = sequelize
